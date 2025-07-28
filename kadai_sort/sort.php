1f3d1522e4e549f6b881b2248affe4d45341b0ec
happy.n5sweet <happy.n5sweet@gmail.com>
2025-07-28 18:11:26 +0900

    降順と昇順

---
+++ kadai_sort/sort.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+         //ここにコードを書いていく
+ 
+       $nums = [15, 4, 18, 23, 10];
+        sort($nums);
+
+        echo '昇順にソートします' .'<br>';
+
+       foreach($nums as $nums){
+        
+        echo $nums .'<br>';
+        }
+
+        
+        $nums = [15, 4, 18, 23, 10];
+        rsort($nums);
+        
+        echo '降順にソートします' .'<br>';
+
+        foreach($nums as $nums){
+        
+        echo $nums .'<br>';
+        }
+
+      
+
+     ?>
+       
+    </p>
+
+</body>
+
+</html>
